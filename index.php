d308ba96ce5e11c03cb87e111a7762ef6ef2d0a5
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-10-29 20:31:05 -0600

    final fo rlogin?

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-md-6 login-left">
                 <h2> Login Here </h2>
-                <form action="validation.php" method="post">
+                <form action="login.php" method="post">
                     <div class="form-group">
                         <label>Username</lable>
                         <input type="text" name="user" class="form-control" >
