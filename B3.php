e00549830fea6f9b5c6853ba749f4b2106fd42ed
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-10-30 15:02:20 -0600

    made it all php

---
+++ B3.php
@@ -0,0 +1,38 @@
+<html>
+    <header>
+        <title>About Us</title>
+        <link rel='shortcut icon' type='image/png' href='favicon.png'/>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </header>
+    <body>
+        <div>
+            <a href=Home.php>
+            <img src="HomeButton.jpg" width="150" height="150" alt="To Home page">
+            </a>
+            <img src="searchbar.png" width="400" height="150" alt="non-working searchbar">
+        </div>
+        <nav>
+            <button onclick="window.location.href='B1.php';">
+                Our Website List A-Z
+              </button>
+
+            <button onclick="window.location.href='B2.php';">
+                Random IP Generator
+              </button>
+
+            <button onclick="window.location.href='B3.php';">
+                About Us
+              </button>
+            <button onclick="window.location.href='B4.php';">
+                Leave A Review
+              </button>
+            </nav>
+        <div>
+            This is who we are and why we were made also all of it is fake get rekt!!#@#%*&^%$#@#$%^&*&^%$#@
+        </div>
+        <footer>
+            <p>Author: Ya Boi Wesley Brown</p>
+            <p>There is no Copy Right cause there is no Copy</p>
+          </footer> 
+    </body>
+</html>
