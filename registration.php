fbd605032ce0efb54cbe69f303ab01d6e2340b2d
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-10-29 20:19:51 -0600

    fix

--- registration.php
+++ registration.php
@@ -9,6 +9,7 @@ mysqli_select_db($con, 'test');
 
 $name = $_POST['user'];
 $pass = $_POST['password'];
+$acc = 0;
 
 $s = "select * from user where name = '$name'";
 
@@ -17,7 +18,7 @@ $result =  mysqli_num_rows($result);
 if($num == 1){
     echo "username taken";
 }else{
-    $reg= "inset into user (name, password) values ('$name', '$pass')";
+    $reg= "inset into user (name, password, access) values ('$name', '$pass', '$acc')";
     mysqli_query($con, $reg);
     echo "Registration Successful";
 }
