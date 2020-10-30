c3494f5fcfa2be82bbdad4e02cfaba47f308fcc8
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-10-29 20:18:55 -0600

    added some more php and sql

--- registration.php
+++ registration.php
@@ -10,6 +10,16 @@ mysqli_select_db($con, 'test');
 $name = $_POST['user'];
 $pass = $_POST['password'];
 
-$s = "select * from user"
+$s = "select * from user where name = '$name'";
+
+$result =  mysqli_num_rows($result);
+
+if($num == 1){
+    echo "username taken";
+}else{
+    $reg= "inset into user (name, password) values ('$name', '$pass')";
+    mysqli_query($con, $reg);
+    echo "Registration Successful";
+}
 
 ?>
