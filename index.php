71456ee53b66b962a6025cde28ed93637c52dc4f
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-10-29 18:05:15 -0600

    The goal here is to use W3 schoolds to make this work

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php include_once("index.html"); ?>
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDBPDO";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // sql to create table
+  $sql = "CREATE TABLE Users (
+        username Varchar(264) Not Null Primary Key,
+        password Varchar(64) Not null,
+        access integer(1))";
+
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Table Users created successfully";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?> 
