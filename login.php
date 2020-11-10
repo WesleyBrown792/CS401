77476884aee76506f992a8e5ed33b6fd25a233ad
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-11-09 21:25:03 -0700

    please dont be a naming convention

--- login.php
+++ login.php
@@ -3,13 +3,15 @@ session_start();
 
 require_once 'Dao.php';
 $dao = new Dao();
-$stmt = $dao-> userExists($_POST['user'], $_POST['password']);
+$stmt = $dao-> userExists($_POST['user'], $_POST['pass']);
+$stmt2 = $dao-> userExists($_POST['user'], $_POST['password']);
 echo print_r($stmt, 1);
+echo print_r($stmt2, 1);
 echo print_r($_SESSION, 1);
 exit();
 
 
-if ($dao->userExists($_POST['user'], $_POST['password'])) {
+if ($dao->userExists($_POST['user'], $_POST['pass'])) {
     $_SESSION['authenticated'] = true;
     header("Location: Home.php");
 } else {
