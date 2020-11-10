d403a5b5f9a6dc012832ed74bc4ad4ba326f6d40
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-11-09 19:13:22 -0700

    failed

--- index.php
+++ index.php
@@ -10,14 +10,12 @@ require_once 'login.php';
 </head>
 
 <body>
-        <div class="login-left">
-            <h2> Login Here </h2>
-            <form method="POST" action="login.php">
-                <div> Username: <input type="text" name="user" require_once/></div>
-                <div> Password: <input type="password" name="pass" require_once/></div>
-                <input type="submit" value="Login">
-            </form>
-        </div>
+        <h2> Login Here </h2>
+        <form method="POST" action="login.php">
+            <div> Username: <input type="text" name="user" require_once/></div>
+            <div> Password: <input type="password" name="pass" require_once/></div>
+            <input type="submit" value="Login">
+        </form>
         <a href='Register.php'> Create An Account Here </a>
 </body>
 </html>
