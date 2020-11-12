0b2da384c1f992c72515022ac39e43514f650b6b
Wesley Brown <wesleybrown792@u.boisestate.edu>
2020-11-11 17:41:05 -0700

    added total adder

--- input.php
+++ input.php
@@ -7,9 +7,10 @@ $url = $_POST['url'];
 $func = $_POST['func'];
 $layout = $_POST['layout'];
 $info = $_POST['info'];
+$total = $info + $func + $layout;
 
 
-$dao->addwebsite($name, $url, $func, $layout, $info, 0);
+$dao->addwebsite($name, $url, $total, $func, $layout, $info, 0);
 
 header('location:Home.php');
 exit();
